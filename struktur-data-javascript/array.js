858483131fb9c67186cc051a762394a36d6389eb
wpgema <134419151+wpgema@users.noreply.github.com>
2023-11-08 22:47:07 +0700

    Delete struktur-data-javascript directory

--- struktur-data-javascript/array.js
+++
@@ -1,7 +0,0 @@
-const arr= [1, 2, true, "gema"];
-
-arr.map(a => console.log(a))
-
-arr.push(arr[1])
-
-arr.map(a => console.log(a))
--- struktur-data-javascript/tes.js
+++
@@ -1,6 +0,0 @@
-let arr1 = [0, 1, 2, 3];
-let arr2 = arr1
-arr2.push(5)
-
-console.log(arr1)
-console.log(arr2)
